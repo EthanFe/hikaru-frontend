4cc1f40fe7ca8b1be8422de89cff615398d35437
EthanFe <sneakcow@gmail.com>
2018-10-16 16:14:41 -0500

    it doesn't do anything yet wooooo

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import WarpCable from 'warp-cable-client'
+window.api = WarpCable('ws://localhost:3000/cable')
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
